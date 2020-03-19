35c729206156b9d50814b91c76d2ec1ec2f2ca19
0x2CA <2478557459@qq.com>
2020-03-19 17:01:37 +0800

    date use date_format

--- themes/maupassant/source/copyright/js/copyright.js
+++ themes/maupassant/source/copyright/js/copyright.js
@@ -1,7 +1,6 @@
 !function (e, t, a) {
     new ClipboardJS('.fa-clipboard');
     $(".fa-clipboard").click(function () {
-        console.log("复制")
         clipboard.on('success', function () {
 
         });
