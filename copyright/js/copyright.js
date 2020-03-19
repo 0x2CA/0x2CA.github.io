92ed2b4c39512b68ea50fbdbb72a232a420227c0
0x2CA <2478557459@qq.com>
2020-03-19 17:39:41 +0800

    Site updated: 2020-03-19 17:39:38

---
+++ copyright/js/copyright.js
@@ -0,0 +1,8 @@
+!function (e, t, a) {
+    new ClipboardJS('.fa-clipboard');
+    $(".fa-clipboard").click(function () {
+        clipboard.on('success', function () {
+
+        });
+    });
+}(window, document);
