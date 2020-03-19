248ed1c13216d9b1d92f7448a760d4212d87b63a
0x2CA <2478557459@qq.com>
2020-03-19 17:39:00 +0800

    Site updated: 2020-03-19 17:38:59

--- copyright/js/copyright.js
+++
@@ -1,8 +0,0 @@
-!function (e, t, a) {
-    new ClipboardJS('.fa-clipboard');
-    $(".fa-clipboard").click(function () {
-        clipboard.on('success', function () {
-
-        });
-    });
-}(window, document);
