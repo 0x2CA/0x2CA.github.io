b8e6781003995f275538fb73377ad74e624883d7
0x2CA <2478557459@qq.com>
2020-03-23 16:04:51 +0800

    Site updated: 2020-03-23 16:04:45

--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 23 2020 15:52:38 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 23 2020 16:04:41 GMT+0800 (GMT+08:00)
 $(window).scroll(function(){$(window).scrollTop()>500?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},500,function(){$("#rocket").removeClass("show launch")});return false});
 //rebuild by neat 
