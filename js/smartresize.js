94141eed8f19e5b408992a9803ee7b90da7885ac
0x2CA <2478557459@qq.com>
2020-03-23 16:39:01 +0800

    Site updated: 2020-03-23 16:38:54

--- js/smartresize.js
+++ js/smartresize.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 23 2020 16:07:59 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 23 2020 16:38:50 GMT+0800 (GMT+08:00)
 +function(i,e){var n=function(i,e,n){var r;return function t(){var t=this,u=arguments;function f(){if(!n){i.apply(t,u)}r=null}if(r){clearTimeout(r)}else if(n){i.apply(t,u)}r=setTimeout(f,e||100)}};jQuery.fn[e]=function(i){return i?this.bind("resize",n(i)):this.trigger(e)}}(jQuery,"smartresize");
 //rebuild by neat 
