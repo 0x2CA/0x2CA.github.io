b5c31ceedc0ce8c65748a1712006a2140aa7e961
0x2CA <2478557459@qq.com>
2020-03-23 16:08:10 +0800

    Site updated: 2020-03-23 16:08:04

--- js/copyright.js
+++ js/copyright.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 23 2020 16:04:41 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 23 2020 16:07:59 GMT+0800 (GMT+08:00)
 !function(t,n,e){var o=document.currentScript||function(){var t=document.getElementsByTagName("script");return t[t.length-1]}();var s=$(o).attr("successtext");var c=new ClipboardJS(".fa-clipboard");c.on("success",function(){if(s){toastr.options={positionClass:"toast-top-center",timeOut:"1000"};toastr.success(s)}})}(window,document);
 //rebuild by neat 
