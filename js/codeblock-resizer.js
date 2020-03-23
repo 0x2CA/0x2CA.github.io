526f8a870d67557510e3dc6f2f3785c23b8e79aa
0x2CA <2478557459@qq.com>
2020-03-23 09:09:53 +0800

    Site updated: 2020-03-23 09:09:46

--- js/codeblock-resizer.js
+++ js/codeblock-resizer.js
@@ -1,3 +1,3 @@
-// build time:Mon Mar 23 2020 09:00:46 GMT+0800 (GMT+08:00)
+// build time:Mon Mar 23 2020 09:09:40 GMT+0800 (GMT+08:00)
 +function(i){"use strict";var t=function(t){this.$codeBlocks=i(t)};t.prototype={run:function(){var t=this;t.resize();i(window).smartresize(function(){t.resize()})},resize:function(){var t=this;t.$codeBlocks.each(function(){var t=i(this).find(".gutter");var r=i(this).find(".code");var n=r.width()-r.innerWidth();var e=i(this).outerWidth()-t.outerWidth()+n;r.css("width",e);r.children("pre").css("width",e)})}};i(document).ready(function(){i.fn.hasHorizontalScrollBar=function(){return this.get(0).scrollWidth>this.innerWidth()};var r=new t("figure.highlight");r.run()})}(jQuery);
 //rebuild by neat 
